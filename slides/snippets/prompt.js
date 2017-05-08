eff37d4823c75c3b48351f2184f8358e667c5967
jlento <jlento@users.noreply.github.com>
2017-05-08 20:41:14 +0300

    Update prompt.js

--- slides/snippets/prompt.js
+++ slides/snippets/prompt.js
@@ -2,8 +2,7 @@ function tag_input() {
     var cmdline=/(^|\n)(\$)([^\n]*)/g;
     [].forEach.call(document.querySelectorAll( "code.sourceCode.bash" ),
                     function (e) {
-                        e.innerHTML = e
-                            .innerHTML
+                        e.innerHTML = e.innerHTML
                             .replace(cmdline, "$1<span class=\"prompt\">$2</span><strong>$3</strong>");
                     });
 }
