1b0acd0436c1621305c0403762fcddba8c5c29ab
Juha Lento <juha.lento@csc.fi>
2018-11-08 10:49:20 +0200

    Prettier filenames

--- Linux2/BashJobManagementAndSignals/create_input_files.sh
+++ Linux2/BashJobManagementAndSignals/create_input_files.sh
@@ -3,6 +3,7 @@
 # Create phony input files for the exercises
 
 for ((i = 0; i < 20; i++)); do
-    mkdir job$i
-    touch job${i}/data${i}_in.dat
+    oi=$(printf "%02d" $i)
+    mkdir job_${oi}
+    touch job_${oi}/data_${oi}_in.dat
 done
