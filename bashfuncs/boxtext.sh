5db288b9d47696c27c60342e9218ba794156a87b
Juha Lento <juha.lento@csc.fi>
2018-12-04 14:35:37 +0200

    Ternary. Re-factor.

--- bashfuncs/boxtext.sh
+++ bashfuncs/boxtext.sh
@@ -5,13 +5,19 @@ hr () {
     echo ${line// /-}
 }
 
-boxtext () {
-    local nx=0 line
-    while read line; do
-        (( ${#line} > nx )) && nx=${#line}
+wc-L () {
+    local nx=0 line n
+    while IFS='' read line; do
+        n=${#line}
+        (( nx = n > nx ? n : nx ))
     done <<<"$1"
+    echo $nx
+}
+
+boxtext () {
+    local nx=$(wc-L "$1")
     hr $((nx+2))
-    while read line; do
+    while IFS='' read line; do
         printf "| %-${nx}s |\n" "$line"
     done <<<"$1"
     hr $((nx+2))
