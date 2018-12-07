481c7f8ea1886081140dfd87e7bc9926f57c551d
Juha Lento <juha.lento@csc.fi>
2018-12-07 19:42:27 +0200

    Input file name to the top, Part 1

--- AOC2018/Dec01.hs
+++ AOC2018/Dec01.hs
@@ -1,10 +1,13 @@
 #!/usr/bin/env stack
 -- stack --resolver lts-12.5 script
 
+fname :: String
+fname = "Dec01.dat"
+
 readInt :: String -> Int
 readInt = read . filter (not . (`elem` "+"))
 
 main :: IO ()
 main = do
-  changes <- fmap ((map readInt) . lines) $ readFile "Dec01.dat"
+  changes <- fmap ((map readInt) . lines) $ readFile fname
   putStrLn $ "Part 1 answer: " ++ show (sum changes)
