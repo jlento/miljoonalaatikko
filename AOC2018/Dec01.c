0fdbf5d0cb8f76439b383350118e8415dc4ef74c
Juha Lento <juha.lento@csc.fi>
2018-12-07 12:02:42 +0200

    Cleaning, Part 1

--- AOC2018/Dec01.c
+++ AOC2018/Dec01.c
@@ -14,11 +14,13 @@ int main(int argc, char *argv[]) {
 
   int64_t value;
   int64_t sum = 0;
-  while (fscanf(file, "%" PRId64, &value ) != EOF) {
+  while (fscanf(file, "%" SCNd64, &value ) != EOF) {
     sum += value;
   }
 
   printf("Part 1 answer: %" PRId64 "\n", sum);
+
+  int64_t seen[1024*256] = {0};
 
   fclose(file);
 
