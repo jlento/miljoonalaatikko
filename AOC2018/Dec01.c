e0566b0962dbc31640efb79d5f207337042c1052
Juha Lento <juha.lento@csc.fi>
2018-12-07 11:57:41 +0200

    Initial commit, Part 1

---
+++ AOC2018/Dec01.c
@@ -0,0 +1,25 @@
+#include <stdint.h>
+#include <inttypes.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+
+  char *fname = "Dec01.dat";
+  FILE *file = fopen(fname, "r");
+
+  if (file == 0) {
+    fprintf(stderr, "Missing input file %s\n", fname);
+    return(1);
+  }
+
+  int64_t value;
+  int64_t sum = 0;
+  while (fscanf(file, "%" PRId64, &value ) != EOF) {
+    sum += value;
+  }
+
+  printf("Part 1 answer: %" PRId64 "\n", sum);
+
+  fclose(file);
+
+}
