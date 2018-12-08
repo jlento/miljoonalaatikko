8c6545601a172dfd918b11d00e1861e78883a862
Juha Lento <juha.lento@csc.fi>
2018-12-08 18:37:16 +0200

    Initial commit, Part 2

--- AOC2018/Dec01.hs
+++ AOC2018/Dec01.hs
@@ -1,13 +1,29 @@
 #!/usr/bin/env stack
--- stack --resolver lts-12.5 script
+-- stack --resolver lts-12.7 script
+
+import qualified Data.IntSet as IntSet
 
 fname :: String
 fname = "Dec01.dat"
 
 readInt :: String -> Int
-readInt = read . filter (not . (`elem` "+"))
+readInt ('+':xs) = read xs
+readInt xs = read xs
+
+findRepeating :: [Int] -> Int
+findRepeating = go IntSet.empty
+  where
+    go _ [] = -1
+    go seen (x:xs)
+      | x `IntSet.member` seen = x
+      | otherwise = go (IntSet.insert x seen) xs
+
 
 main :: IO ()
 main = do
+
   changes <- fmap ((map readInt) . lines) $ readFile fname
+  let partialSums = scanl (+) 0 (cycle changes)
+
   putStrLn $ "Part 1 answer: " ++ show (sum changes)
+  putStrLn $ "Part 2 answer: " ++ show (findRepeating partialSums)
