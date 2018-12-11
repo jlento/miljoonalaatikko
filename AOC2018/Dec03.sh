d47b894efc422265eb1fa2bcfdb8fd5a1335ec76
Juha Lento <juha.lento@csc.fi>
2018-12-11 22:51:27 +0200

    Initial commit, part 1, bash

--- AOC2018/Dec03.sh
+++ AOC2018/Dec03.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-nall=0
 sed -E -e 's/^.*@ //' -e 's/(,|: |x)/ /g' "${0%.*}.dat" | \
     sort -n -k 1 | \
     while read x y nx ny; do
-        ((nall += nx * ny))
         for ((i = 0; i < nx; i++)); do
             for ((j = 0; j < ny; j++)); do
                 echo $((x + i)) $((y + j))
