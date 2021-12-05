5875f071a002af6b9382c0aa4925fa712aa98e15
Juha Lento <juha.lento@csc.fi>
2021-12-05 12:54:33 +0200

    Initial commit

---
+++ AOC2021/dec01-1.c
@@ -0,0 +1,36 @@
+//usr/bin/env cc "$0" && exec ./a.out "$0" "$@"
+
+#include <stdint.h>
+#include <inttypes.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+
+  if (argc < 3) {
+    goto usage;
+  }
+
+  FILE *file = fopen(argv[2], "r");
+
+  if (file == 0) {
+    goto missing_input_file;
+  }
+
+  int64_t current, previous = INT64_MIN, counter = -1;
+  while (fscanf(file, "%" SCNd64, &current) != EOF) {
+    if (current > previous) counter++;
+    previous = current;
+  }
+
+ success:
+  printf("%" PRId64 "\n", counter);
+  return(0);
+
+ usage:
+  fprintf(stderr, "Usage: %s <input file>\n", argv[1]);
+  return(1);
+
+ missing_input_file:
+  fprintf(stderr, "Error. Cannot open input file '%s'\n", argv[2]);
+  return(1);
+}
