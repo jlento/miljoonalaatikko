eb2c182bf14481fc4c919715143cfa3e34b415cc
Juha Lento <juha.lento@csc.fi>
2021-12-05 16:11:06 +0200

    Update.

--- AOC2021/dec01-2.c
+++ AOC2021/dec01-2.c
@@ -4,6 +4,8 @@
 #include <inttypes.h>
 #include <stdio.h>
 
+#define MAXLEN 1024
+
 int main(int argc, char *argv[]) {
 
   if (argc < 3) {
@@ -16,13 +18,14 @@ int main(int argc, char *argv[]) {
     goto missing_input_file;
   }
 
-  int64_t current, previous = 0, counter = -1;
-  while (fscanf(file, "%" SCNd64, &current) != EOF) {
-    if (current > previous) counter++;
-    previous = current;
+  int64_t s = 0, r0 = 0, r1 = 0, r2 = 0, counter = -3;
+  while (fscanf(file, "%" SCNd64, &r2) != EOF) {
+    if(r0 + r1 + r2 > s) counter++;
+    s = r0 +r1 + r2;
+    r0 = r1; r1 =r2;
   }
   fclose(file);
-  
+
  success:
   printf("%" PRId64 "\n", counter);
   return(0);
