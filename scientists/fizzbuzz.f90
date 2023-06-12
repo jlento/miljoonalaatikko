6a797a8b9bfd55080a51c3a15170ea4b79a5e5e4
Juha Lento <juha.lento@csc.fi>
2023-06-12 12:39:57 +0300

    Initial commit

---
+++ scientists/fizzbuzz.f90
@@ -0,0 +1,49 @@
+program fizzbuzz
+  implicit none
+  integer :: i
+
+  do i = 1, 21
+     write (*, '(2A10)') iffy(i), mathy(i)
+  end do
+
+contains
+
+  function iffy(i) result (str)
+
+    integer, intent(in) :: i
+    character (len = 10) :: str
+
+    if (mod(i, 3) == 0) then
+       if (mod(i, 5) == 0) then
+          str = 'FizzBuzz'
+       else
+          str = 'Fizz'
+       end if
+    else if (mod(i, 5) == 0) then
+       str = 'Buzz'
+    else
+       write (str, '(G0)') i
+    end if
+
+  end function iffy
+
+
+  function mathy(i) result (str)
+
+    integer, intent(in) :: i
+    character (len = 10) :: str
+
+    select case (mod(i, 15))
+    case (0)
+       str = 'FizzBuzz'
+    case (3, 6, 9, 12)
+       str = 'Fizz'
+    case (5, 10)
+       str = 'Buzz'
+    case default
+       write (str, '(G0)') i
+    end select
+
+  end function mathy
+
+end program fizzbuzz
